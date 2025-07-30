0e6dc1cbfd286c936442783c5866040ebf5765fd
nothinDD <andriekusbenediktas@gmail.com>
2025-07-30 15:56:09 +0300

    no need for comms

--- scrapper.py
+++ scrapper.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 url="https://autoplius.lt/skelbimai/naudoti-automobiliai"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)  # sometimes headless=True is easier detected
+    browser = p.chromium.launch(headless=True)
     context = browser.new_context(
         user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
                    "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -13,11 +13,9 @@ with sync_playwright() as p:
         locale="en-US",
         java_script_enabled=False,
         bypass_csp=True,
-        # Optionally enable storage like cookies, cache
     )
     page = context.new_page()
     page.goto(url)
-    # page.wait_for_load_state("networkidle")  # wait for all requests
     html = page.content()
     soup=BeautifulSoup(html, 'html.parser')
     print(soup.prettify())
