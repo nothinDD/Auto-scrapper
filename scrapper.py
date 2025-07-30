e9981f175a316f1d2a4f25a61950d5436d380587
nothinDD <andriekusbenediktas@gmail.com>
2025-07-30 15:55:50 +0300

    bypass bot check

--- scrapper.py
+++ scrapper.py
@@ -5,9 +5,20 @@ from playwright.sync_api import sync_playwright
 url="https://autoplius.lt/skelbimai/naudoti-automobiliai"
 
 with sync_playwright() as p:
-    browser=p.chromium.launch(headless=True)
-    page = browser.new_page()
+    browser = p.chromium.launch(headless=True)  # sometimes headless=True is easier detected
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                   "AppleWebKit/537.36 (KHTML, like Gecko) "
+                   "Chrome/114.0.0.0 Safari/537.36",
+        locale="en-US",
+        java_script_enabled=False,
+        bypass_csp=True,
+        # Optionally enable storage like cookies, cache
+    )
+    page = context.new_page()
     page.goto(url)
-    soup=BeautifulSoup(str(page))
-    print(soup)
+    # page.wait_for_load_state("networkidle")  # wait for all requests
+    html = page.content()
+    soup=BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
     browser.close()
