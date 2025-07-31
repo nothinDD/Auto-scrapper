53c8adb3437d4cddbae202e13faa6feed08ec0b9
nothinDD <andriekusbenediktas@gmail.com>
2025-07-31 00:46:59 +0000

    Completed the whole workflow, needs file integration, fix the e vehicles, proper storing, more modules

--- scrapper.py
+++ scrapper.py
@@ -17,6 +17,9 @@ def get_cars(html:str)->Tuple[List[str],str]:
     next_page=url+soup.find("a", {"class" : "next"}).get("href")
 
     return car_links,next_page
+def get_info(car_html:str):
+    return None
+
 
 if __name__ == "__main__":
     with sync_playwright() as p:
@@ -26,14 +29,43 @@ if __name__ == "__main__":
                        "AppleWebKit/537.36 (KHTML, like Gecko) "
                        "Chrome/114.0.0.0 Safari/537.36",
             locale="en-US",
-            java_script_enabled=False,
+            java_script_enabled=True,
             bypass_csp=True,
         )
         page = context.new_page()
         page.goto(url)
         html = page.content()
 
-        car_lists,next_page=get_cars(html)
-        print(car_lists)
-        print(next_page)
+        page_count=1
+        car_count=0
+
+        while page is not None:
+            page_count+=1
+            car_lists,next_page=get_cars(html)
+            for car in car_lists:
+                page.goto(car)
+
+                html=page.content()
+                soup = BeautifulSoup(html, 'html.parser')
+
+                parameter_block=soup.find("div", {"class": "row announcement-section"})
+
+                parameter_names=parameter_block.find_all("div", {"class": "parameter-label"})
+                parameter_values=parameter_block.find_all("div", {"class": "parameter-value"})
+
+                par=[parameter.string.strip() for parameter in parameter_names if parameter.string.strip()!='Kėbulo numeris (VIN)']
+                par_values=[parameter.string.replace('\n','').strip()
+                            for parameter in parameter_values if parameter.string is not None]
+
+                car_object=dict(zip(par,par_values))
+
+                print(car_object)
+
+            car_count += len(car_lists)
+            page.goto(next_page)
+
+            print(f"\nGoing to next page: {page_count}\n")
+            print(f"\nCurrent scrapped car count:{car_count}\n")
+
+            html=page.content()
         browser.close()
