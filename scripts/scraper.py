3019412a47532b53af2a2adafb4e1fefc067e510
nothinDD <andriekusbenediktas@gmail.com>
2025-08-02 23:01:43 +0000

    ocd

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -10,6 +10,7 @@ user=("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
                        "AppleWebKit/537.36 (KHTML, like Gecko) "
                        "Chrome/114.0.0.0 Safari/537.36")
 file = "results.csv"
+
 car_dict={"Kaina":[],
           "Pirma registracija": [],
           "Rida": [],
